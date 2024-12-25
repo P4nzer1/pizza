91dc69bc449871530047036e950188928a2cafee
Nikita Smirov <nikitka-smirnov-2013@mail.ru>
2024-12-25 19:55:20 +0300

    fsg-20

--- pizza/src/app/App.tsx
+++ pizza/src/app/App.tsx
@@ -1,20 +1,7 @@
-import { Suspense } from "react"
-import { Provider } from "react-redux"
-import { RouterProvider } from "react-router-dom";
 
-import router from "./router/router";
-import store from "./store";
-//fallback={<Spinner />}
 function App() {
   return (
     <>
-      <Provider store={store}>
-        <Suspense >
-          <RouterProvider router={router} />
-        </Suspense>
-
-      </Provider>
-
     </>
   )
 }
--- pizza/src/shared/api/constant.ts
+++
@@ -1,3 +0,0 @@
-export const BASE_URL = 'http://localhost:5000'; 
-
-export const AUTH_URL = 'http://localhost:5000/api/auth';
