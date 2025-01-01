4e85dde28f2e722e2fb9532cf865124f3eba1b83
Nikita Smirov <nikitka-smirnov-2013@mail.ru>
2025-01-01 16:00:47 +0300

    финальые правки

--- pizza/src/app/App.tsx
+++ pizza/src/app/App.tsx
@@ -1,16 +1,6 @@
-import { useState } from "react"
-
-import Modal from "@/shared/ui/Modal/Modal"
-
-
 function App() {
-  const [isOpen,setIsOpen] = useState(false)
-  const onClose = () => setIsOpen(false)
-  const onOpen = () => setIsOpen(true)
   return (
     <>
-      <button onClick={onOpen}>Авторизация</button>
-      <Modal isOpen={isOpen} onClose={onClose}/>
     </>
   )
 }
