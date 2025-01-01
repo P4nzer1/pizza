04619418178e6585d0b500b0a75f58f599e03b35
Nikita Smirov <nikitka-smirnov-2013@mail.ru>
2025-01-01 17:13:29 +0300

    правки 01/01/2025

--- pizza/src/app/App.tsx
+++ pizza/src/app/App.tsx
@@ -1,11 +1,5 @@
 function App() {
-  const [isOpen,setIsOpen] = useState(false)
   
-  const onClose = () => setIsOpen(false)
-  const onOpen = () => setIsOpen(true)
-
-  
-
   return (
     <>
     </>
--- pizza/src/shared/ui/Input/Input.tsx
+++ pizza/src/shared/ui/Input/Input.tsx
@@ -1,6 +1,8 @@
 import classNames from "classnames";
-import styles from "./Input.module.scss";
 import { InputHTMLAttributes } from "react";
+
+import styles from "./Input.module.scss";
+
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   className?: string;
